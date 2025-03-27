a13667297d0472696ae8dab32cadcad94c7f83ed
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-03-27 10:53:01 +0530

    App3

---
+++ App3/app/src/main/java/com/example/app3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.app3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button btnNext;
+        btnNext=findViewById(R.id.btnnext);
+        Intent iNext;
+        iNext=new Intent(MainActivity.this, SecondActivity.class);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(iNext);
+            }
+        });
+
+        };
+    }
