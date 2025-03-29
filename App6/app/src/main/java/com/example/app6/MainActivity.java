625c5a1f7ce8ccb6615c469d2f1157881cc4f1a1
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-03-30 02:45:04 +0530

    App6

--- App6/app/src/main/java/com/example/app6/MainActivity.java
+++ App6/app/src/main/java/com/example/app6/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.app6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,46 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txtAnim;
+Button Translate,Alpha,Rotate,Scale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtAnim=findViewById(R.id.text);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.move);
+        Translate=findViewById(R.id.btnTranslate);
+        Alpha=findViewById(R.id.btnAlpha);
+        Rotate=findViewById(R.id.btnRotate);
+        Scale=findViewById(R.id.btnScale);
+        Translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation move=AnimationUtils.loadAnimation(MainActivity.this,R.anim.move);
+                txtAnim.startAnimation(move);
+            }
+        });
+        Alpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation alpha=AnimationUtils.loadAnimation(MainActivity.this,R.anim.move);
+                txtAnim.startAnimation(alpha);
+            }
+        });
+        Rotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation rotate=AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate);
+                txtAnim.startAnimation(rotate);
+            }
+        });
+        Scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation scale=AnimationUtils.loadAnimation(MainActivity.this,R.anim.scale);
+                txtAnim.startAnimation(scale);
+            }
         });
     }
 }
