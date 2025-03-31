2528dbf4b83b2b8cd91b82691b8b77bb196246a1
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-01 00:35:04 +0530

    App8

--- App8/app/src/main/java/com/example/app8/MainActivity.java
+++ App8/app/src/main/java/com/example/app8/MainActivity.java
@@ -4,21 +4,21 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+CardView card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        card=findViewById(R.id.card);
+        card.setCardElevation(5.0f);
+        card.setRadius(11.0f);
+        card.setUseCompatPadding(true);
     }
 }
