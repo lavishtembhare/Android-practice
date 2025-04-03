ac1aa457d62ed38167e7d74fd12443a898a1c968
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-04 00:05:40 +0530

    App11

--- App11/app/src/main/java/com/example/app11/MainActivity.java
+++ App11/app/src/main/java/com/example/app11/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.app11;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,21 +10,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-TextView text;
-Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.text);
-        b1=findViewById(R.id.b1);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,Second_Activity.class);
-                startActivityForResult(i,2);
-            }
-        });
+        Toast.makeText(getApplicationContext(),"This is custom toast",Toast.LENGTH_SHORT).show();
     }
 }
