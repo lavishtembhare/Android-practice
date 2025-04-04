0894e6fb00979cda6a7f93ce65afd1ba5e06c43c
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-04 12:39:19 +0530

    App11

--- App11/app/src/main/java/com/example/app11/MainActivity.java
+++ App11/app/src/main/java/com/example/app11/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.app11;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import static com.example.app11.R.*;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"This is custom toast",Toast.LENGTH_SHORT).show();
+
+        // Get the button from the layout
+        Button toastBtn = findViewById(R.id.toast_btn);
+
+        // Set the button's OnClickListener
+        toastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the custom toast
+                Toast toast = new Toast(getApplicationContext());
+                View view = getLayoutInflater().inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.toastView));
+                toast.setView(view);
+
+                // Set message in TextView
+                TextView txtMsg = view.findViewById(R.id.txtMsg);
+                txtMsg.setText("Custom Toast");
+
+                // Show toast with short duration
+                toast.setDuration(LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
