44af7f5eae5334cf32ac86dcd45fff61544cf8cd
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-07 02:41:02 +0530

    App14

--- App14/app/src/main/java/com/example/app14/MainActivity.java
+++ App14/app/src/main/java/com/example/app14/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app14;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn_call,btn_dialer,btn_msg,btn_email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn_call=findViewById(R.id.btn_call);
+        btn_dialer=findViewById(R.id.btn_dialer);
+        btn_email=findViewById(R.id.btn_email);
+        btn_msg=findViewById(R.id.btn_msg);
+        btn_dialer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iDial=new Intent();
+                iDial.setAction(Intent.ACTION_DIAL);
+                iDial.setData(Uri.parse("tel: +918554043545"));
+                startActivity(iDial);
+            }
+        });
+        btn_msg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
         });
     }
 }
