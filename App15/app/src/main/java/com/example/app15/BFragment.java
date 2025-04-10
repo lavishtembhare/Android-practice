e5241031c434edab5d072a8ae121160f9d88c64d
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-10 19:41:52 +0530

    App15

--- App15/app/src/main/java/com/example/app15/BFragment.java
+++ App15/app/src/main/java/com/example/app15/BFragment.java
@@ -1,7 +1,9 @@
 package com.example.app15;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +11,10 @@ import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AFragment#newInstance} factory method to
+ * Use the {@link BFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
-public class AFragment extends Fragment {
+public class BFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,26 +25,26 @@ public class AFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public BFragment() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AFragment.
+     * @return A new instance of fragment BFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AFragment newInstance(String param1, String param2) {
-        AFragment fragment = new AFragment();
+    public static BFragment newInstance(String param1, String param2) {
+        BFragment fragment = new BFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public AFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -60,7 +61,7 @@ public class AFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_a,container,false);
-        TextView textView_a=view.findViewById(R.id.txtFrag);
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        TextView textView_b=view.findViewById(R.id.txtFrag1);
+        return inflater.inflate(R.layout.fragment_b, container, false);
     }
 }
