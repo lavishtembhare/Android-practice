2cb6473f9866c9a1b814385aa212a1617060b60b
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-11 03:05:46 +0530

    App16

--- App15/app/src/main/java/com/example/app15/MainActivity.java
+++ App15/app/src/main/java/com/example/app15/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-Button b1,b2,b3;
+    Button b1,b2,b3;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,7 @@ Button b1,b2,b3;
         b1=findViewById(R.id.btnfragA);
         b2=findViewById(R.id.btnfragB);
         b3=findViewById(R.id.btnfragC);
-        loadFrag(new BFragment(),0);
+        loadFrag(new AFragment(),0);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,9 +50,13 @@ Button b1,b2,b3;
         FragmentTransaction ft=fm.beginTransaction();
         if(flag==0){
             ft.add(R.id.container, fragment);
+            fm.popBackStack(ROOT_FRAGMENT_TAG,FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.addToBackStack(ROOT_FRAGMENT_TAG);
         }else{
             ft.replace(R.id.container, fragment);
+            ft.addToBackStack(null);
         }
+
         ft.commit();
     }
 }
