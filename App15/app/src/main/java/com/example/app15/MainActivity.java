0e5f3c41102dfa785d3b3ae8f0677122ccfc9016
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-11 01:49:02 +0530

    App15

--- App15/app/src/main/java/com/example/app15/MainActivity.java
+++ App15/app/src/main/java/com/example/app15/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.app15;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b1,b2,b3;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b1=findViewById(R.id.btnfragA);
+        b2=findViewById(R.id.btnfragB);
+        b3=findViewById(R.id.btnfragC);
+        loadFrag(new BFragment(),0);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new AFragment(),1);
+            }
         });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new BFragment(),1);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFrag(new CFragment(),1);
+            }
+        });
+    }
+    public void loadFrag(Fragment fragment,int flag){
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        if(flag==0){
+            ft.add(R.id.container, fragment);
+        }else{
+            ft.replace(R.id.container, fragment);
+        }
+        ft.commit();
     }
 }
