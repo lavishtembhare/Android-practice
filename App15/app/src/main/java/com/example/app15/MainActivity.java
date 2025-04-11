29fbb9a3bc02227199961741742561549f2c0bd4
Lavish Tembhare <lavishtembhare93@gmail.com>
2025-04-11 10:52:20 +0530

    App15

--- App15/app/src/main/java/com/example/app15/MainActivity.java
+++ App15/app/src/main/java/com/example/app15/MainActivity.java
@@ -7,56 +7,80 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2,b3;
+    private static final String ROOT_FRAGMENT_TAG = "root_fragment";
+
+    Button b1, b2, b3;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        b1=findViewById(R.id.btnfragA);
-        b2=findViewById(R.id.btnfragB);
-        b3=findViewById(R.id.btnfragC);
-        loadFrag(new AFragment(),0);
+
+        b1 = findViewById(R.id.btnfragA);
+        b2 = findViewById(R.id.btnfragB);
+        b3 = findViewById(R.id.btnfragC);
+
+        // Load the root fragment
+        loadFrag(new AFragment(), 0);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFrag(new AFragment(),1);
+                loadFrag(new AFragment(), 1);
             }
         });
+
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFrag(new BFragment(),1);
+                loadFrag(new BFragment(), 1);
             }
         });
+
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFrag(new CFragment(),1);
+                loadFrag(new CFragment(), 1);
             }
         });
     }
-    public void loadFrag(Fragment fragment,int flag){
-        FragmentManager fm=getSupportFragmentManager();
-        FragmentTransaction ft=fm.beginTransaction();
-        if(flag==0){
-            ft.add(R.id.container, fragment);
-            fm.popBackStack(ROOT_FRAGMENT_TAG,FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+    public void loadFrag(Fragment fragment, int flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (flag == 0) {
+            // Clear the back stack up to and including the root fragment
+            fm.popBackStack(ROOT_FRAGMENT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.replace(R.id.container, fragment);
             ft.addToBackStack(ROOT_FRAGMENT_TAG);
-        }else{
+        } else {
             ft.replace(R.id.container, fragment);
             ft.addToBackStack(null);
         }
 
         ft.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        FragmentManager fm = getSupportFragmentManager();
+        int backStackCount = fm.getBackStackEntryCount();
+
+        if (backStackCount > 1) {
+            // Pop the current fragment
+            fm.popBackStack();
+        } else {
+            // Only the root fragment is left
+            finish(); // Exit the app
+        }
     }
 }
